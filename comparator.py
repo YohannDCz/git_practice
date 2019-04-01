d86066e2fc50957ecb8cc60e8142c814f12eb598
Yohann Di Crescenzo <YohannDCz@gmail.com>
2019-04-01 12:28:51 +0200

    Le latest version

--- comparator.py
+++ comparator.py
@@ -20,51 +20,47 @@ def userInput(userInput):
         return userInput
   
 def groundShipping(pound):
+    cost = 0
     if pound <= 2:
         cost = pound * 1.5
-    elif pound > 2:
+    elif pound <= 6:
         cost = pound * 3
-    elif pound > 6:
+    elif pound <= 10:
         cost = pound * 4
     elif pound > 10:
         cost = pound * 4.75
     cost += 20
-    
+    return cost
+
+
 def droneShipping(pound):
+    cost = 0
     if pound <= 2:
         cost = pound * 4.5
-    elif pound > 2:
+    elif pound <= 6:
         cost = pound * 9
-    elif pound > 6:
+    elif pound <= 10:
         cost = pound * 12
     elif pound > 10:
-    cost = pound * 14.25
+        cost = pound * 14.25
+    return cost
 
 def premiumShipping(pound):
     cost = 125
-  
-def comparator(pound):
-    cost = 0
-    x = groundShipping(pound)
-    y = droneShipping(pound)
-    z = premiumShipping(pound)
-    if x == y and y == z:
-    return x
-    elif x > y:
-    if x < z:
-        return y
-    else:
-        return z 
-    elif y > z:
-    if y < x:
-        return z
-    else:
-        return x 
-    elif z > x:
-    if z < y:
-        return x
-    else:
-        return y
     return cost
 
-  comparator(5)
+def comparator(pound):
+    groundShippingg = groundShipping(pound)
+    droneShippingg = droneShipping(pound)
+    premiumShippingg = premiumShipping(pound)
+    if groundShippingg == droneShippingg and droneShippingg == premiumShippingg:
+        return "You should use groundShipping. It will cost $" +str(groundShipping(pound))+ "." 
+    elif groundShippingg > premiumShippingg and droneShippingg > premiumShippingg: 
+        return "You should use premium shipping. It will cost $" +str(premiumShipping(pound))+ "."
+    elif groundShippingg > droneShippingg and premiumShippingg > droneShippingg:
+        return "You should use drone shipping. It will cost $" +str(droneShippingg(pound))+ "."
+    elif premiumShippingg > groundShippingg and droneShippingg > groundShippingg:
+        return "You should use ground shipping. It will cost $" +str(groundShipping(pound))+ "."
+
+print(groundShipping(6))
+print(comparator(5))
