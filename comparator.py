a2ebe85e17946a2fb4e734461238950401fd10d2
Yohann Di Crescenzo <YohannDCz@gmail.com>
2019-04-01 12:03:45 +0200

    Made by myself

--- comparator.py
+++ comparator.py
@@ -14,55 +14,57 @@
 # return the cheapest price
 
 def userInput(userInput):
-  if type(userInput) != int:
-    return "You must input a number!"
-  else:
-    return userInput
+    if type(userInput) != int:
+        return "You must input a number!"
+    else:
+        return userInput
   
 def groundShipping(pound):
-  if pound <= 2:
-    cost = pound * 1.5
-  elif pound > 2:
-    cost = pound * 3
-  elif pound > 6:
-    cost = pound * 4
-  elif pound > 10:
-    cost = pound * 4.75
-  cost += 20
+    if pound <= 2:
+        cost = pound * 1.5
+    elif pound > 2:
+        cost = pound * 3
+    elif pound > 6:
+        cost = pound * 4
+    elif pound > 10:
+        cost = pound * 4.75
+    cost += 20
     
 def droneShipping(pound):
-  if pound <= 2:
-    cost = pound * 4.5
-  elif pound > 2:
-    cost = pound * 9
-  elif pound > 6:
-    cost = pound * 12
-  elif pound > 10:
+    if pound <= 2:
+        cost = pound * 4.5
+    elif pound > 2:
+        cost = pound * 9
+    elif pound > 6:
+        cost = pound * 12
+    elif pound > 10:
     cost = pound * 14.25
 
 def premiumShipping(pound):
-  cost = 125
+    cost = 125
   
 def comparator(pound):
-  cost = 0
-  x = groundShipping(pound)
-  y = droneShipping(pound)
-  z = premiumShipping(pound)
-  if x == y and y == z:
+    cost = 0
+    x = groundShipping(pound)
+    y = droneShipping(pound)
+    z = premiumShipping(pound)
+    if x == y and y == z:
     return x
-  elif x > y:
-    if x < z: zxy
-      return y
-    else: xyz
-      return z 
-  elif y > z:
-    if y < x: xyz
-      return z
-    else: yzx
-      return x 
-  elif z > x:
-    if z < y:
-      return x
+    elif x > y:
+    if x < z:
+        return y
     else:
-      return y
-  return cost
+        return z 
+    elif y > z:
+    if y < x:
+        return z
+    else:
+        return x 
+    elif z > x:
+    if z < y:
+        return x
+    else:
+        return y
+    return cost
+
+  comparator(5)
